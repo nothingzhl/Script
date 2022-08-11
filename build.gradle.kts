94e161b02819288221d195901aee0f57cffacfb1
zhanghanlin <zhanghanlin02@huohua.cn>
2022-08-11 19:00:59 +0800

    初始化主流程

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.antlr:antlr4-runtime:4.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
